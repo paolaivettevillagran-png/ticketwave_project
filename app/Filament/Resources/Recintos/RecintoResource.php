0cd0daa0049e76f3ae686527d3bf95d6f7005a86
Paola Ivette <paolaivettevillagran@gmail.com>
2026-05-13 08:39:01 -0600

    feat(TW-19): agrega navigationGroup y navigationSort a RecintoResource

--- app/Filament/Resources/Recintos/RecintoResource.php
+++ app/Filament/Resources/Recintos/RecintoResource.php
@@ -16,6 +16,8 @@ use Filament\Tables\Table;
 
 class RecintoResource extends Resource
 {
+    protected static ?string $navigationGroup = 'Principal';
+    protected static ?int $navigationSort = 6;
     protected static ?string $model = Recinto::class;
 
     protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedRectangleStack;
